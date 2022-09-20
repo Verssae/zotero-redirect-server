440ad217af1ad22d9a2b254be3789171ce4ca711
Hansae Ju <sparky@hanyang.ac.kr>
2022-09-20 13:00:59 +0900

    remove example

--- main.js
+++ main.js
@@ -3,11 +3,6 @@ var open = require('open')
 
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-
 app.get('/:collection/:item', function (req, res) {
   res.redirect(`zotero://select/library/collections/${req.params.collection}/items/${req.params.item}`);
   console.log('Zotero item opened');
@@ -15,6 +10,6 @@ app.get('/:collection/:item', function (req, res) {
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Redirect app listening on port 3000!');
 });
 
