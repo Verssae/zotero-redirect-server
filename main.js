230ff5dba745dfb31b88adc79e18b353c0f00dce
Hansae Ju <sparky@hanyang.ac.kr>
2022-09-20 12:57:54 +0900

    change open to redirect

--- main.js
+++ main.js
@@ -3,10 +3,16 @@ var open = require('open')
 
 var app = express();
 
-app.get('/:collection/:item', function (req, res) {
-  res.send('<script>window.close();</script>');
-  open(`zotero://select/library/collections/${req.params.collection}/items/${req.params.item}`);
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
+
+
+app.get('/:collection/:item', function (req, res) {
+  res.redirect(`zotero://select/library/collections/${req.params.collection}/items/${req.params.item}`);
+  console.log('Zotero item opened');
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
